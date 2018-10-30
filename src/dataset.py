91680d02513b5fb5ec3d9c7645e148181b0f3b5f
WangHexie <31768052+WangHexie@users.noreply.github.com>
2018-10-30 14:51:23 +0800

    git add .

--- src/dataset.py
+++ src/dataset.py
@@ -9,27 +9,51 @@ from scipy.sparse import vstack as s_vstack
 import sys
 import itertools
 
-
 Datasets = collections.namedtuple('Datasets', ['train', 'test', 'embeddings', 'node_cluster',
-                                                'labels', 'idx_label', 'label_name'])
+                                               'labels', 'idx_label', 'label_name'])
+time_used = 0
+
+
+def time_cost(func):
+    """
+    @time_cost  放在任意函数定义的上一行，运行时便能自动打印出函数所用时间
+    :param func:
+    :return:
+    """
+
+    def wrapper(*args, **kw):
+        import time
+        start_time = time.time()
+        result = func(*args, **kw)
+        finish_time = time.time()
+        print("used_time(s):", finish_time - start_time)
+        global time_used
+        time_used += finish_time - start_time
+        return result
+
+    return wrapper
+
 
 class DataSet(object):
-
+    time_used = 0
     def __init__(self, edge, nums_type, **kwargs):
         self.edge = edge
-        self.edge_set = set(map(tuple, edge)) ### ugly code, need to be fixed
+        self.edge_set = set(map(tuple, edge))  ### ugly code, need to be fixed
         self.nums_type = nums_type
         self.kwargs = kwargs
         self.nums_examples = len(edge)
         self.epochs_completed = 0
         self.index_in_epoch = 0
 
+    @time_cost
     def next_batch(self, embeddings, batch_size=16, num_neg_samples=1, pair_radio=0.9, sparse_input=True):
         """
             Return the next `batch_size` examples from this data set.
             if num_neg_samples = 0, there is no negative sampling.
         """
         while 1:
+            import time
+            start_time = time.time()
             start = self.index_in_epoch
             self.index_in_epoch += batch_size
             if self.index_in_epoch > self.nums_examples:
@@ -44,7 +68,7 @@ class DataSet(object):
                 ### warning !!! we need deepcopy to copy list
                 index = copy.deepcopy(self.edge[i])
                 n_neg = 0
-                while(n_neg < num_neg_samples):
+                while (n_neg < num_neg_samples):
                     mode = np.random.rand()
                     if mode < pair_radio:
                         type_ = np.random.randint(3)
@@ -64,7 +88,7 @@ class DataSet(object):
                 batch_data = np.vstack((self.edge[start:end], neg_data))
                 nums_batch = len(batch_data)
                 labels = np.zeros(nums_batch)
-                labels[0:end-start] = 1
+                labels[0:end - start] = 1
                 perm = np.random.permutation(nums_batch)
                 batch_data = batch_data[perm]
                 labels = labels[perm]
@@ -73,14 +97,18 @@ class DataSet(object):
                 nums_batch = len(batch_data)
                 labels = np.ones(len(batch_data))
             batch_e = embedding_lookup(embeddings, batch_data, sparse_input)
+            finish_time = time.time()
+            DataSet.time_used = DataSet.time_used + finish_time - start_time
             yield (dict([('input_{}'.format(i), batch_e[i]) for i in range(3)]),
-                    dict([('decode_{}'.format(i), batch_e[i]) for i in range(3)]+[('classify_layer', labels)]))
+                   dict([('decode_{}'.format(i), batch_e[i]) for i in range(3)] + [('classify_layer', labels)]))
+
 
 def embedding_lookup(embeddings, index, sparse_input=True):
     if sparse_input:
-        return [embeddings[i][index[:, i], :].todense()  for i in range(3)]
+        return [embeddings[i][index[:, i], :].todense() for i in range(3)]
     else:
-        return [embeddings[i][index[:, i], :]  for i in range(3)]
+        return [embeddings[i][index[:, i], :] for i in range(3)]
+
 
 def read_data_sets(train_dir):
     TRAIN_FILE = 'train_data.npz'
@@ -98,15 +126,19 @@ def read_data_sets(train_dir):
     del data
     embeddings = generate_embeddings(train_data.edge, train_data.nums_type)
     return Datasets(train=train_data, test=test_data, embeddings=embeddings, node_cluster=node_cluster,
-                labels=labels, idx_label=idx_label, label_name=label_set)
+                    labels=labels, idx_label=idx_label, label_name=label_set)
+
 
 def generate_H(edge, nums_type):
     nums_examples = len(edge)
-    H = [csr_matrix((np.ones(nums_examples), (edge[:, i], range(nums_examples))), shape=(nums_type[i], nums_examples)) for i in range(3)]
+    H = [csr_matrix((np.ones(nums_examples), (edge[:, i], range(nums_examples))), shape=(nums_type[i], nums_examples))
+         for i in range(3)]
     return H
 
+
 def dense_to_onehot(labels):
-    return np.array(map(lambda x: [x*0.5+0.5, x*-0.5+0.5], list(labels)), dtype=float)
+    return np.array(map(lambda x: [x * 0.5 + 0.5, x * -0.5 + 0.5], list(labels)), dtype=float)
+
 
 def generate_embeddings(edge, nums_type, H=None):
     if H is None:
